37f7e74a3b3e8ff3ad06b20df07fbd925deb78a4
Taupin Fabien <taupinfabien2607@gmail.com>
2023-05-11 15:30:39 +0200

    second push

--- YU-GI-OH/src/App.jsx
+++ YU-GI-OH/src/App.jsx
@@ -1,18 +1,79 @@
-import './App.css'
-import './conponents/Header'
-import Header from './conponents/Header'
-import Footer from './conponents/Footer'
-import Should_you_buy from './conponents/Should_you_buy'
+import "./App.css";
+import "./conponents/Header";
+import Header from "./conponents/Header";
+import Footer from "./conponents/Footer";
+import Should_you_buy from "./conponents/Should_you_buy";
 
 function App() {
-
   return (
     <>
-    <Header />
-    <Should_you_buy />
-    <Footer />
+      <Header />
+      <Should_you_buy />
+      <Footer />
+
+      <div className="page">
+        <div className="left_page">
+          <h1>YU-GI-OH</h1>
+          <img
+            src="../public/assets/image20.png"
+            className="img20"
+            alt="image de yugi"
+          ></img>
+          <div className="p">
+            <p>
+              Duel Monsters is the central plot of Yu-Gi-Oh. A fictionnal game
+              where each player uses cards to "duel" each other in a mock battle
+              of fantasy "monsters"
+            </p>
+          </div>
+          <img
+            src="../public/assets/image 22.png"
+            className="img22"
+            alt="image de godzilla"
+          ></img>
+        </div>
+        <div className="page_right">
+          <div className="text_page">
+            <div>
+              <strong>The rules are simple: the first one to lose dies.</strong>
+            </div>
+            <p>Kazuki Takahashi, creator of Yu-Gi-Oh !</p>
+          </div>
+          <div className="images">
+            <img
+              src="../public/assets/image21.png"
+              className="image21"
+              alt="image_de_dragon"
+            ></img>
+            <img
+              src="../public/assets/image23.png"
+              className="image23"
+              alt="image_de_dragon"
+            ></img>
+          </div>
+          <div className="details">
+            <div>
+              <p>
+                The fictional card game discovered by thousands of children
+                through the manga and anime, has become a staple of trading card
+                games.
+              </p>
+            </div>
+            <div>
+              <p>
+                With the game's popularity, anyone can become a skilled player
+                and participate in countless duels. In the game, players draw
+                cards from their decks and take turns playing them on the field,
+                using a deck of 40-60 cards and an optional "Extra Deck" of up
+                to 15 cards.
+              </p>
+            </div>
+            
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
