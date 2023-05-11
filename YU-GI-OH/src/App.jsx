5cbcd2462a70a564a9c4c8ee1abf21b1223ba6a6
maianabertola <maia@we-are-ensemble.com>
2023-05-11 14:32:45 +0200

    encart done card details done also

--- YU-GI-OH/src/App.jsx
+++ YU-GI-OH/src/App.jsx
@@ -1,18 +1,21 @@
-import './App.css'
-import './conponents/Header'
-import Header from './conponents/Header'
-import Footer from './conponents/Footer'
-import Should_you_buy from './conponents/Should_you_buy'
+import "./App.css";
+import "./conponents/Header";
+import Header from "./conponents/Header";
+import Footer from "./conponents/Footer";
+import Should_you_buy from "./conponents/Should_you_buy";
+import Callout from "./conponents/Callout";
+import CardDetails from "./conponents/CardDetails";
 
 function App() {
-
   return (
     <>
-    <Header />
-    <Should_you_buy />
-    <Footer />
+      {/* <Header /> */}
+      <CardDetails></CardDetails>
+      {/* <Callout></Callout>
+      <Should_you_buy />
+      <Footer /> */}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ YU-GI-OH/src/conponents/CardDetails.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import card from "../assets/card template.jpeg";
+import "./CardDetails.css";
+
+function CardDetails() {
+  return (
+    <div className="cardDetailsContainer">
+      <div className="imgContainer">
+        <div className="cardShadow">
+          <img src={card} />
+        </div>
+      </div>
+
+      <div className="infoContainer">
+        <div className="titleCard">
+          <h1>Name of the card</h1>
+          <hr></hr>
+        </div>
+        <div className="row">
+          <div className="item">Attribute: Wind</div>
+          <div className="item">Race: Nain de jardin</div>
+        </div>
+
+        <div className="row">
+          <div className="item">Attack: 1200</div>
+          <div className="item">Defense: 1400</div>
+        </div>
+
+        <div className="row item">
+          <article>
+            2 Level 4 monsters\nOnce per turn Quick Effect: You can detach 1
+            material from this card, then target 1 Spell/Trap on the field;
+            destroy it
+          </article>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default CardDetails;
