ddf4755e9196181cf3fbd688022d9a279bd93d25
maianabertola <maia@we-are-ensemble.com>
2023-05-11 18:23:29 +0200

    html almost done

--- YU-GI-OH/src/App.jsx
+++ YU-GI-OH/src/App.jsx
@@ -3,30 +3,27 @@ import "./conponents/Header";
 import Header from "./conponents/Header";
 import Footer from "./conponents/Footer";
 import Should_you_buy from "./conponents/Should_you_buy";
-
-import Description_Page from "./conponents/Description_Page"
-
+import Description_Page from "./conponents/Description_Page";
 import Callout from "./conponents/Callout";
 import CardDetails from "./conponents/CardDetails";
 import Form from "./conponents/Form";
-
+import Question from "./conponents/Question";
 
 function App() {
   return (
     <>
-
-      <Header />
+      {/* <Header />
       <Description_Page />
       <Should_you_buy />
-      <Footer />      
+      <Footer />       */}
 
       {/* <Header /> */}
       {/* <CardDetails></CardDetails> */}
-      <Form></Form>
+      {/* <Form></Form> */}
+      <Question></Question>
       {/* <Callout></Callout>
       <Should_you_buy />
       <Footer /> */}
-
     </>
   );
 }
