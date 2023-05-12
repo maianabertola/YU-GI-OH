430dc4b03442b7bfaa500254293a185efb024123
maianabertola <maia@we-are-ensemble.com>
2023-05-12 15:07:41 +0200

    voila

--- YU-GI-OH/src/pages/QuestionDefense.jsx
+++ YU-GI-OH/src/pages/QuestionDefense.jsx
@@ -1,16 +1,26 @@
 import React from "react";
+import { useOutletContext } from "react-router-dom";
+import Button from "../conponents/Button";
+import "../conponents/Form.css";
+import robot from "../assets/yugioh robot.png";
 
-function QuestionDefense({ defense, setDefense, handleDefense }) {
+function QuestionDefense() {
+  const {
+    defense: [defense, setDefense, handleDefense],
+  } = useOutletContext();
+
   return (
     <>
       <div className="formContainer">
         <div className="imageContainer">
-          <img src="" alt="alt"></img>
+          <img src={robot} alt="robot yu gi oh"></img>
         </div>
 
         <div className="textFormContainer"></div>
         <div className="inputContainer">
-          <label htmlFor="defense">What's its power of attack?</label>
+          <label htmlFor="defense">
+            How many defense points do you want to assign to your monster?
+          </label>
           <input
             type="number"
             id="defense"
@@ -18,6 +28,7 @@ function QuestionDefense({ defense, setDefense, handleDefense }) {
             onChange={(event) => setDefense(event.target.value)}
           />
         </div>
+        <Button onClick={handleDefense} cta={"Last question"}></Button>
       </div>
     </>
   );
